7b471f39019e5c20008e1a3a11c7b2da37c05ccc
vedamehar <vedamehar@gmail.com>
2024-12-22 07:31:37 +0000

    container config done

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 145533
+let accountEmail ="vedantmehar@gmail.com"
+var accountPassword ="21212121"
+accountCity ="Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail ="vd@vd.com"
+accountPassword ="12210548"
+accountCity="Bengarulu"
+
+/*
+Prefer not to use 
+because in issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
