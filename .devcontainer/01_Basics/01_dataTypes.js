eff3fbab3c01ceb7acac9fd00492a90ffa4d5aa6
vedamehar <vedamehar@gmail.com>
2024-12-22 08:09:54 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3
+    +
+     3) // code readability should be high
+
+console.log("Vedant")
+let name = "Vedant"
+let age = 19
+let isloggedIn = false
+let State = null
+
+// number => 2 to pwer 53
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
