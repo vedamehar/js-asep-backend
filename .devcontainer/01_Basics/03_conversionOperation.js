b7640e3ee5e354aa2b35974f4ce60319444ab3a8
vedamehar <vedamehar@gmail.com>
2024-12-23 17:35:05 +0000

    conversion Operations

--- .devcontainer/01_Basics/03_conversionOperation.js
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -22,7 +22,44 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 //"Vedant" => true
 
 let someNumber = 33
-
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+// ******************** Operations ********************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1 = "hello ";
+let str2 = "Vedant";
+
+
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
